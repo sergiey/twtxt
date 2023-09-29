adddedd1574e4ae0aecff5911967691cc6b78c3a
Sergei Melnikov <melnikhov@gmail.com>
2023-09-29 23:24:56 +0300

    Add MakeTweet method.

--- Twtxt.cs
+++ Twtxt.cs
@@ -1,10 +1,37 @@
+using twtxt.Models;
+
 namespace twtxt;
 
 public class Twtxt
 {
-    public static string Help()
+    private readonly string _textFile = "twtxt.txt";
+
+    public void MakeTweet(string text)
     {
-        return
+        Tweet tw = new Tweet(text);
+        FileInfo fi = new FileInfo(_textFile);
+
+        if(fi.Exists)
+        {
+            using StreamWriter sw = fi.AppendText();
+            sw.WriteLine($"{tw.AbsoluteDateTime}\t{tw.Text}");
+        }
+        else
+        {
+            fi.Create();
+            using StreamWriter sw = fi.CreateText();
+            sw.WriteLine($"{tw.AbsoluteDateTime}\t{tw.Text}");
+        }
+
+    }
+
+    public void ShowTimeline()
+    {
+    }
+
+    public void ShowHelp()
+    {
+        Console.WriteLine(
             """
             Usage:
                     ./twtxtc [COMMAND]
@@ -17,6 +44,6 @@ public class Twtxt
                                             <user> defines the user name to display.
                     unfollow <user>         Removes the user with the display name <user> from your timeline.
                     help                    Displays this help screen.
-            """;
+            """);
     }
 }
