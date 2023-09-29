f023a83d0ab7926eb8c5b76d5e222483089b97f3
Sergei Melnikov <melnikhov@gmail.com>
2023-09-29 19:03:47 +0300

    Add base program function.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using twtxt;
+using twtxt.Models;
+using static twtxt.Manual;
+
+internal class Program
 {
     public static void Main(string[] args)
     {
-
+        if (args.Length < 1) {
+            Console.WriteLine(Help());
+            return;
+        }
+        args[0].ToLower() switch
+        {
+            "tweet"     => MakeTweet(args[1]),
+            "timeline"  => ShowTimeline(),
+            "following" => ShowFollowing(),
+            "follow"    => Follow(args[1], args[2]),
+            "unfollow"  => Unfollow(args[1]),
+            "help"      => Help(),
+            "man"       => Help(),
+            _           => Help()
+        };
     }
 }
