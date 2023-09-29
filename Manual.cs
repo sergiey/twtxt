f56f24b067876b466671e6aa24d9f890a5e1a9cb
Sergei Melnikov <melnikhov@gmail.com>
2023-09-29 18:42:49 +0300

    Add Manual class.

--- Manual.cs
+++ Manual.cs
@@ -2,5 +2,21 @@ namespace twtxt;
 
 public class Manual
 {
-    
+    public static string Help()
+    {
+        return
+            """
+            Usage:
+                    ./twtxtc [COMMAND]
+
+            Commands:
+                    tweet <text>            Adds <text> to your twtxt timeline.
+                    timeline                Displays your twtxt timeline.
+                    following               Gives you a list of all people you follow.
+                    follow <user> <URL>     Adds the twtxt file from <URL> to your timeline.
+                                            <user> defines the user name to display.
+                    unfollow <user>         Removes the user with the display name <user> from your timeline.
+                    help                    Displays this help screen.
+            """;
+    }
 }
